f7a1477bc38a1d8d61e4f9cb1ee615c04e35e3a1
kirinnee <kirinnee97@gmail.com>
2023-11-13 14:37:58 +0800

    fix: package name too close to existing package

--- sdks/python/cyan_sdk/api/plugin/req.py
+++
@@ -1,8 +0,0 @@
-from typing import Dict, Any
-
-from cyan_sdk.api.base_model import CyanBaseModel
-
-
-class PluginReq(CyanBaseModel):
-    directory: str
-    config: Dict[str, Any]
--- sdks/python/cyan_sdk/api/plugin/res.py
+++
@@ -1,5 +0,0 @@
-from cyan_sdk.api.base_model import CyanBaseModel
-
-
-class PluginRes(CyanBaseModel):
-    output_dir: str
